35fd77dbcb67c51dbe138bbf53432f7fa58bd5e1
nathaalves <nathaalvesaraujo@gmail.com>
2022-04-04 22:39:11 -0300

    Implementada a limitação de entrada para a quantidade de cartas

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-const numberOfCards = prompt("Com quantas cartas você quer jogar?")
+let numberOfCards = prompt("Com quantas cartas você quer jogar?")
+
+while (numberOfCards%2 !== 0 || numberOfCards < 2 || numberOfCards > 14) {
+
+    alert("Você deve inserir um número par de 2 a 14")
+    numberOfCards = prompt("Com quantas cartas você quer jogar?")
+
+}
