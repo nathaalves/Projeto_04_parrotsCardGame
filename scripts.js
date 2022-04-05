1f255aacb39b687d9faa06c51bdb3225de9c6ed8
nathaalves <nathaalvesaraujo@gmail.com>
2022-04-04 23:42:21 -0300

    Inserção das cartas de acordo com a quantidade especificada pelo jogador

--- scripts.js
+++ scripts.js
@@ -6,3 +6,21 @@ while (numberOfCards%2 !== 0 || numberOfCards < 2 || numberOfCards > 14) {
     numberOfCards = prompt("Com quantas cartas você quer jogar?")
 
 }
+
+for (let i = 1; i <= numberOfCards; i++) {
+
+    let divElement = document.createElement("div")
+    divElement.setAttribute("class", "card")
+    document.querySelector(".container").appendChild(divElement);
+    
+}
+
+const cards = document.querySelectorAll(".card")
+
+for (let i = 0; i < cards.length; i++) {
+
+    let imgElement = document.createElement("img")
+    imgElement.setAttribute("src", "images/front.png")
+    cards[i].appendChild(imgElement);
+    
+}
