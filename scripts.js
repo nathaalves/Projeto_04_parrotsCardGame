aa1776d77381e42ec58798771e0c59d7a687de29
nathaalves <nathaalvesaraujo@gmail.com>
2022-04-23 00:57:42 -0300

    correção de bug: função declarada dentro de função

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
 
-let timer
-const cardsContainer = document.querySelector(".container")
-const clock = document.querySelector(".stopwatch")
+let timer;
+const cardsContainer = document.querySelector(".container");
+const clock = document.querySelector(".stopwatch");
 
-startGame()  
+startGame();
 
 function startGame() {
 
-    let numberOfCards = prompt("Com quantas cartas você quer jogar?")
+    let numberOfCards = prompt("Com quantas cartas você quer jogar?");
 
     while (numberOfCards%2 !== 0 || numberOfCards < 2 || numberOfCards > 14) {
 
-        alert("Você deve inserir um número par de 2 a 14")
-        numberOfCards = prompt("Com quantas cartas você quer jogar?")
+        alert("Você deve inserir um número par de 2 a 14");
+        numberOfCards = prompt("Com quantas cartas você quer jogar?");
     }
 
-    let cards = []
+    let cards = [];
     for (let i = 0; i < numberOfCards/2; i++) {
 
         for (let j = 0; j < 2; j++) {
-            cards.push(`images/img${i}.gif`)
+            cards.push(`images/img${i}.gif`);
         }
     }
 
@@ -37,114 +37,116 @@ function startGame() {
             <div class="cardFace back">
                 <img src="images/front.png" alt="">
             </div>
-        </div>`
+        </div>`;
     }
 
-    timer = setInterval(timeCounter, 1000)
+    timer = setInterval(timeCounter, 1000);
 }
 
-let unflipedCards = []
-let unflipedCount = 0
-let playCount = 0
+let unflipedCards = [];
+let unflipedCount = 0;
+let playCount = 0;
 
 function flipCard(fliped) {
 
-    playCount += 1
+    playCount += 1;
 
-    fliped.classList.remove("fliped")
-    unflipedCards[unflipedCount] = fliped
-    unflipedCount += 1
+    fliped.classList.remove("fliped");
+    unflipedCards[unflipedCount] = fliped;
+    unflipedCount += 1;
 
-    fliped.setAttribute("onclick", "")
+    fliped.setAttribute("onclick", "");
 
     if (unflipedCount === 2) {
         
-        const temp1 = unflipedCards[0]
-        const temp2 = unflipedCards[1]
-        const img1 = temp1.querySelector("img").src
-        const img2 = temp2.querySelector("img").src
+        const temp1 = unflipedCards[0];
+        const temp2 = unflipedCards[1];
+        const img1 = temp1.querySelector("img").src;
+        const img2 = temp2.querySelector("img").src;
 
         if (img1 !== img2) {
 
-            setTimeout(flipCardsBack, 1000)
-            function flipCardsBack () {
-                
-                temp1.classList.add("fliped")
-                temp1.setAttribute("onclick", "flipCard(this)")
-                temp2.classList.add("fliped")
-                temp2.setAttribute("onclick", "flipCard(this)")
-            }
+            setTimeout(flipCardsBack, 1000);
+            flipCardsBack()
         }
         
-        unflipedCount = 0
+        unflipedCount = 0;
     }
 
-    setTimeout(win, 500)
+    setTimeout(win, 500);
 }
 
-let sec = 0
-let min = 0
-let hour = 0
+function flipCardsBack () {
+                
+    temp1.classList.add("fliped");
+    temp1.setAttribute("onclick", "flipCard(this)");
+    temp2.classList.add("fliped");
+    temp2.setAttribute("onclick", "flipCard(this)");
+}
+
+let sec = 0;
+let min = 0;
+let hour = 0;
 
 function timeCounter () {
 
-    sec++
+    sec++;
 
     if (sec === 60) {
-        min++
-        sec = 0
+        min++;
+        sec = 0;
     }
 
     timerSec = (sec < 10) ? "0" + sec : sec
 
     if (min === 60) {
-        hour++
-        min = 0
+        hour++;
+        min = 0;
     }
     
     timerMin = (min < 10) ? "0" + min : min
 
     if (hour > 24) {
-        hour = 0
+        hour = 0;
     }
 
-    timerHour = (hour < 10) ? "0" + hour : hour
+    timerHour = (hour < 10) ? "0" + hour : hour;
 
-    clock.innerHTML = `${timerHour}:${timerMin}:${timerSec}`
+    clock.innerHTML = `${timerHour}:${timerMin}:${timerSec}`;
 }
 
 function win() {
 
     if(document.querySelectorAll(".fliped").length === 0) {    
 
-        clearInterval(timer)
+        clearInterval(timer);
 
-        let winMensage = ""
+        let winMensage = "";
 
         if (hour > 0) {
-            winMensage = `${hour} horas, ${min} minutor e ${sec} segundos`
+            winMensage = `${hour} horas, ${min} minutor e ${sec} segundos`;
         } else if (min > 0){
-            winMensage = `${min} minutor e ${sec} segundos`
+            winMensage = `${min} minutor e ${sec} segundos`;
         } else {
-            winMensage = `${sec} segundos`
+            winMensage = `${sec} segundos`;
         }
 
-        alert(`Você venceu em ${playCount} jogadas, com um tempo de ${winMensage}`)
+        alert(`Você venceu em ${playCount} jogadas, com um tempo de ${winMensage}`);
 
-        let answer = prompt("Você quer jogar novamente? responda com 'sim' ou 'não'")
+        let answer = prompt("Você quer jogar novamente? responda com 'sim' ou 'não'");
 
         while (answer !== "sim" && answer !== "não") {
-            alert("A única opções de resposta é 'sim' ou 'não'")
-            answer = prompt("Você quer jogar novamente? responda com 'sim' ou 'não'")
+            alert("A única opções de resposta é 'sim' ou 'não'");
+            answer = prompt("Você quer jogar novamente? responda com 'sim' ou 'não'");
         }
 
         if (answer === "sim") {
 
-            cardsContainer.innerHTML = ""
-            clock.innerHTML = "00:00:00"
-            playCount = 0
-            sec = 0
-            startGame()
+            cardsContainer.innerHTML = "";
+            clock.innerHTML = "00:00:00";
+            playCount = 0;
+            sec = 0;
+            startGame();
         }
     }
 }
